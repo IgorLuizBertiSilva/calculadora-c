e8644d84b3dc94b00de0839baa9a96847e282854
IgorLuizBertiSilva <igor.luiz@aluno.ifsp.edu.br>
2023-05-16 11:44:17 -0300

    Feat: Agora faz as operacoes no estilo suico

--- main.c
+++ main.c
@@ -1,5 +1,83 @@
 #include <stdio.h>
 
+int calcular(float valores[], char operacao[], int tamOperacoes){
+    float resultado;
+    int j = 1;
+
+    for (int i = 0; i < tamOperacoes; i++)
+    {
+
+        printf("%c\n", operacao[i]);
+        if (operacao[i] == '+')
+        {
+            valores[0] = valores[0] + valores[j];
+        }
+        else if (operacao[i] == '-')
+        {
+            valores[0] = valores[0] - valores[j];
+        }
+        else if (operacao[i] == '*')
+        {
+            valores[0] = valores[0] * valores[j];
+        }
+        else if (operacao[i] == '/')
+        {
+            valores[0] = valores[0] / valores[j];
+        }
+        else
+        {
+            printf("Operacao invalida!\n");
+            return NULL;
+        }
+        j++;
+    
+    }
+
+    return valores[0];
+    
+}
+
+float teste(x, y){
+    return x + y;
+}
+
+int calculadora(int tammanhoValores, int tamanhoOperacoes){
+    float valores[tammanhoValores];
+    char operacao[tamanhoOperacoes];
+    float resultado;
+
+    for (int i = 0; i < tammanhoValores; i++)
+    {
+        printf("Digite o valor %d: ", i+1);
+        scanf("%f", &valores[i]);
+    }
+
+    for (int i = 0; i < tamanhoOperacoes; i++)
+    {
+        printf("Digite a operacao %d: ", i+1);
+        scanf("%c", &operacao[i]);
+        scanf("%c", &operacao[i]);
+    }
+
+    resultado = calcular(valores, operacao, tamanhoOperacoes);
+
+    printf("Resultado: %f", resultado);
+    
+    return 0;
+}
+
+
 int main(){
-    printf("Hello World!\n");
+    
+    int tamOperacoes, tamValores;
+
+    printf("Digite o quantidade dos valores: ");
+    scanf("%d", &tamValores);
+
+    tamOperacoes = tamValores - 1;
+
+    calculadora(tamValores, tamOperacoes);
+    
+    return 0;
+
 }
